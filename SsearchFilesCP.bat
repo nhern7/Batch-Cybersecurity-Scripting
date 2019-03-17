00158cf5747a91aa436995fa2a0860e97cc50b8e
Naomi <47608592+nhern7@users.noreply.github.com>
2019-03-17 13:55:16 -0400

    Add files via upload

---
+++ SsearchFilesCP.bat
@@ -0,0 +1,108 @@
+@echo OFF
+title badfileSearchCP
+echo This is the start of the search.
+pause
+echo.
+
+echo ~~start of mp3 file search~~.
+for /R "C:\Users\ " %%G in (*.MP3) do (
+	
+	echo %%G  
+	
+)
+echo ~~end of mp3 file search~~.
+echo.
+echo ~~start of jpg file search~~.
+for /R "C:\Users\ " %%F in (*.JPG) do (
+	
+	echo %%F  
+	
+)
+echo ~~end of jpg file search~~.
+echo.
+echo ~~start of mp4 file search~~.
+for /R "C:\Users\ " %%H in (*.MP4) do (
+	
+	echo %%H  
+	
+)
+echo ~~end of mp4 file search~~.
+echo.
+echo ~~start of png file search~~.
+for /R "C:\Users\ " %%E in (*.PNG) do (
+	
+	echo %%E  
+	
+)
+echo ~~end of png file search~~.
+echo.
+echo ~~start of ac3 file search~~.
+for /R "C:\Users\ " %%L in (*.AC3) do (
+	
+	echo %%L  
+	
+)
+echo ~~end of ac3 file search~~.
+echo.
+echo ~~start of ac3 file search~~.
+for /R "C:\Users\ " %%L in (*.AC3) do (
+	
+	echo %%L  
+	
+)
+echo ~~end of ac3 file search~~.
+echo.
+echo ~~start of aac file search~~.
+for /R "C:\Users\ " %%P in (*.AAC) do (
+	
+	echo %%P  
+	
+)
+echo ~~end of aac file search~~.
+echo.
+echo ~~start of ac3 file search~~.
+for /R "C:\Users\ " %%B in (*.aiff) do (
+	
+	echo %%B  
+	
+)
+echo ~~end of aiff file search~~.
+echo.
+echo ~~start of m4a file search~~.
+for /R "C:\Users\ " %%O in (*.m4a) do (
+	
+	echo %%O  
+	
+)
+echo ~~end of m4a file search~~.
+echo.
+echo ~~start of m4a file search~~.
+for /R "C:\Users\ " %%K in (*.m3a) do (
+	
+	echo %%K  
+	
+)
+echo ~~end of m3a file search~~.
+echo.
+echo ~~start of wav file search~~.
+for /R "C:\Users\ " %%Q in (*.wav) do (
+	
+	echo %%Q  
+	
+)
+echo ~~end of wav file search~~.
+echo.
+echo ~~start of m4a file search~~.
+for /R "C:\Users\ " %%Z in (*.jpeg) do (
+	
+	echo %%Z  
+	
+)
+echo ~~end of jpeg file search~~.
+echo.
+echo This is the end of the search. Can now put bad file addresses into address bar to find them.
+pause
+
+
+
+                     
